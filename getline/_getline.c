997e58f81294922aa5dab135f53ea22a0f9b7738
Tanner Saint <tsaint2004@gmail.com>
2024-09-05 04:41:33 +0000

    fixed EOF detection

--- getline/_getline.c
+++ getline/_getline.c
@@ -22,10 +22,18 @@ char *_getline(const int fd)
     while (1)
     {
         bytes_read = read(fd, buffer, READ_SIZE);
-        if (bytes_read <= 0)
+        if (bytes_read < 0)
         {
             free(line);
             return NULL;
+        }
+
+        if (bytes_read == 0) {
+            if (total_size == 0) {
+                free(line);
+                return NULL;
+            }
+            break;
         }
 
         if (total_size + bytes_read >= buffer_size)
