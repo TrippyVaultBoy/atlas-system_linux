ec2f1b288a3db54747e75082831daabd752a5983
Tanner Saint <tsaint2004@gmail.com>
2024-09-12 16:10:19 +0000

    fixed when directory name prints

--- ls/main.c
+++ ls/main.c
@@ -10,27 +10,29 @@ int main(int argc, char *argv[]) {
     struct dirent *read;
     int i;
 
-    for (i = 1; i < argc; i++)
-    {
-        printf("%s:\n", argv[i]);
+    for (i = 1; i < argc; i++) {
+		if (argc > 2)
+			printf("%s:\n", argv[i]);
 
         dir = opendir(argv[i]);
-        if (dir == NULL)
-        {
+        
+		if (dir == NULL) {
             perror("unable to open directory");
 			return -1;
         }
 
-		while ((read = readdir(dir)) != NULL)
-		{
-			if (read->d_name[0] == '.')
-			{
+		while ((read = readdir(dir)) != NULL) {
+			if (read->d_name[0] == '.') {
 				continue;
 			}
 
         	printf("%s ", read->d_name);	
 		}
+		
 		printf("\n");
+
+		if (argc == 1)
+			break;
     }
 
     closedir(dir);
