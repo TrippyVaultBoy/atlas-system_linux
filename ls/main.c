90da7413258964b91d407730c610e9ac3912c69b
Tanner Saint <tsaint2004@gmail.com>
2024-09-17 23:09:07 +0000

    added filetype check

--- ls/main.c
+++ ls/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
 /**
  * main- program entry point
  *
@@ -8,31 +10,46 @@
 int main(int argc, char *argv[]) {
     DIR *dir;
     struct dirent *read;
+	struct stat fileStat;
     int i;
 
     for (i = 1; i < argc; i++) {
-		if (argc > 2)
-			printf("%s:\n", argv[i]);
-
-        dir = opendir(argv[i]);
-        
-		if (dir == NULL) {
-            perror("unable to open directory");
+		if (lstat(argv[i], &fileStat) == -1)
+		{
+			perror("lstat error");
 			return -1;
-        }
+		}
 
-		while ((read = readdir(dir)) != NULL) {
-			if (read->d_name[0] == '.') {
-				continue;
+		if (S_ISREG(fileStat.st_mode))
+		{
+			printf("%s\n", argv[i]);
+			return 0;
+		}
+		else if (S_ISDIR(fileStat.st_mode))
+		{
+			if (argc > 2)
+				printf("%s:\n", argv[i]);
+
+        	dir = opendir(argv[i]);
+        
+			if (dir == NULL) {
+				perror("unable to open directory");
+				return -1;
 			}
 
-        	printf("%s ", read->d_name);	
-		}
-		
-		printf("\n");
+			while ((read = readdir(dir)) != NULL) {
+				if (read->d_name[0] == '.') {
+					continue;
+				}
 
-		if (argc == 1)
-			break;
+				printf("%s ", read->d_name);	
+			}
+		
+			printf("\n");
+
+			if (argc == 1)
+				break;
+		}
     }
 
     closedir(dir);
