2ee89b3c9dbf198ae90f0c6bceb0c9a63cf29654
Tanner Saint <tsaint2004@gmail.com>
2024-10-28 21:59:55 +0000

    refactored 1-strcmp.asm

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -1,29 +1,21 @@
 section .text
     global asm_strcmp
 
-asm_strcmp:
-    xor rax, rax
+    asm_strcmp:
+    .loop:
+        mov al, byte [rdi]
+        mov bl, byte [rsi]
 
-.loop:
-    mov al, [rdi]
-    mov bl, [rsi]
+        cmp al, bl
+        jne .return
 
-    cmp al, bl
-    jne .not_equal
+        test al, al
+        je .return
 
-    test al, al
-    je .equal
+        inc rdi
+        inc rsi
+        jmp .loop
 
-    inc rdi
-    inc rsi
-    jmp .loop
-
-.not_equal:
-    movzx eax, al
-    movzx ebx, bl
-    sub eax, ebx
-    ret
-
-.equal:
-    xor rax, rax
-    ret
+    .return:
+        sub eax, ebx
+        ret
