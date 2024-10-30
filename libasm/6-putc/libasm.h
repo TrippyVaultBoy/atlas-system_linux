d06290e1cf7544e5b55dbf2f2e70cbdcebb8e53e
Tanner Saint <tsaint2004@gmail.com>
2024-10-30 07:29:00 +0000

    saving progress

---
+++ libasm/6-putc/libasm.h
@@ -0,0 +1,14 @@
+#ifndef LIBASM_H
+#define LIBASM_H
+
+#include <stddef.h>
+
+extern size_t asm_strlen(const char *str);
+extern int asm_strcmp(const char *s1, const char *s2);
+extern int asm_strncmp(const char *s1, const char *s2, size_t n);
+extern char *asm_strchr(const char *s, int c);
+extern char *asm_strstr(const char *haystack, const char *needle);
+extern void *asm_memcpy(void *dest, const void *src, size_t n);
+extern void *asm_memcpy(void *dest, const void *src, size_t n);
+
+#endif
