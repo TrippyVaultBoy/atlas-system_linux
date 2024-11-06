a95e5cfb6d33e74d36abfb733ac2f893c3c7f808
Tanner Saint <tsaint2004@gmail.com>
2024-11-06 21:32:25 +0000

    made betty complaiant

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -5,9 +5,9 @@ void (*current_handler_signal(void))(int)
 	void (*handler)(int) = signal(SIGINT, SIG_IGN);
 
 	if (handler == SIG_IGN)
-		return NULL;
+		return (NULL);
 
 	signal(SIGINT, handler);
 
-	return handler;
+	return (handler);
 }
