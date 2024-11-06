d68855e280e03bd3c6a7da1e87b8254584441d2f
Tanner Saint <tsaint2004@gmail.com>
2024-11-06 21:34:07 +0000

    added documentation

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -1,5 +1,11 @@
 #include "signals.h"
 
+/**
+* current_handler_signal - retrieves the current handler of the signal SIGINT
+*
+* Return: void
+*/
+
 void (*current_handler_signal(void))(int)
 {
 	void (*handler)(int) = signal(SIGINT, SIG_IGN);
