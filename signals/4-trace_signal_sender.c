07e927a6daca858df1f3f8419e746436502d295d
Tanner Saint <tsaint2004@gmail.com>
2024-11-06 22:29:28 +0000

    added documentation

--- signals/4-trace_signal_sender.c
+++ signals/4-trace_signal_sender.c
@@ -1,13 +1,24 @@
 #include "signals.h"
 
+/**
+* handle_sigquit - handles sigquit
+*
+* Return: void
+*/
 void handle_sigquit(int signum, siginfo_t *info, void *context)
 {
 	(void)context;
-	
+
 	if (signum == SIGQUIT)
 		printf("SIGQUIT sent by %d\n", info->si_pid);
 }
 
+
+/**
+* trace_signal_sender - defines a handler for the signal SIGQUIT
+*
+* Return: 0 or -1
+*/
 int trace_signal_sender(void)
 {
 	struct sigaction sa;
