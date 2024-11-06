0e3f6da7e4fd264330fb359c8bddd0ba8648d2fe
Tanner Saint <tsaint2004@gmail.com>
2024-11-06 22:09:46 +0000

    made betty complaint

--- signals/3-current_handler_sigaction.c
+++ signals/3-current_handler_sigaction.c
@@ -1,6 +1,6 @@
 #include "signals.h"
 /**
-* current_handler_sigaction - retrieves the current 
+* current_handler_sigaction - retrieves the current
 * handler of the signal SIGINT
 *
 * Return: NULL or pointer
